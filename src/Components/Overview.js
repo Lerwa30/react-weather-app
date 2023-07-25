9c3e100dd0823498121ead9c461a0f9d40fd7862
Leroy <leroypawlick@gmail.com>
2023-07-24 19:38:58 -0700

    added functionality to weather api to display avg country weather

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { useSelector, UseSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
+
 const Overview = () => {
-    return <div>Overview</div>
+    const currentDisplay = useSelector(selectDisplay);
+    return <div className="stack">
+        <h1>{currentDisplay.name.common}</h1>
+        <h2>Capital: {currentDisplay.capital}</h2>
+        <h3>Population: {currentDisplay.population}</h3>
+    </div>
 };
 
 export default Overview;
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,8 +1,34 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useSelector, UseSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
 
 const Weather = () => {
     const [weather, setWeather] = useState();
+
+    const display = useSelector(selectDisplay);
+
+    const latitude = display.capitalInfo.latlng[0]
+    const longitude = display.capitalInfo.latlng[1]
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+            params: {q: `${latitude}, ${longitude}`},
+            headers: {
+              'X-RapidAPI-Key': '15e3f37ac0msh75219e693469049p12457djsnc0ce39e06112',
+              'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+            }
+          };
+          
+         axios
+          .request(options)
+          .then(function (response) {
+            console.log(response.data)
+            setWeather(response.data)
+          })
+    }, [])
 
     // ------------------------------------
     // PASTE RAPIDAPI CODE SNIPPET IN A USEEFFECT HERE
@@ -13,18 +39,26 @@ const Weather = () => {
             <table className="overview-table">
                 <tr>
                     <td>Conditions: </td>
+                    <td>{weather?.current?.condition?.text}</td>
                 </tr>
                 <tr>
                     <td>Temperature: </td>
+                    <td>{weather?.current.temp_f} degrees Fahrenheit</td>
                 </tr>
                 <tr>
                     <td>Feels Like: </td>
+                    <td>{weather?.current?.feelslike_f} degrees Fahrenheit</td>
                 </tr>
                 <tr>
                     <td>Humidity: </td>
+                    <td>{weather?.current?.humidity}%</td>
                 </tr>
                 <tr>
                     <td>Wind Speed: </td>
+                    <td>
+                        {weather?.current?.wind_mph} mph{" "}
+                        {weather?.current?.wind_dir}
+                    </td>
                 </tr>
             </table>
         </div>
